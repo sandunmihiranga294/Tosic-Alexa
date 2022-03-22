42cf33c43e5fc67c31bb081977f12ef0511d5ca7
Sandun1mihiranga <101900336+Sandun1Mihiranfa@users.noreply.github.com>
2022-03-23 00:55:42 +0530

    Update info.js

--- plugins/info.js
+++ plugins/info.js
@@ -29,7 +29,7 @@ Asena.addCommand({pattern: 'info', fromMe: false, desc: 'Shows bot information a
 │❖ *ᴀᴜᴛʜᴏʀ* : ` + Config.PLK + `
 │❖ *ᴋᴇʀɴᴇʟ* : ʟɪɴᴜx
 │❖ *ɪɴꜱᴛᴀɢʀᴀᴍ* : ` + Config.SLINK + `
-│❖ *OWNER* : https:/wa.me/916380260672...
+│❖ *OWNER* : https:/wa.me/94761905764...
 ╰────────────────❋ཻུ۪۪➹
 ⌕ ❙❘❙❙❘❙❚❙❘❙❙❚❙❘❙❘❙❚❙❘❙❙❚❙❘❙❙❘❙❚❙❘
                ` + Config.BOTSK + `
